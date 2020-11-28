0acfcecbb6a77f0c5e685c8b1a46e6f79d904d25
Knowledge Kwaramba <knowhowkwaramba@gmail.com>
2020-11-28 10:37:01 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,5 @@
-
+require('dotenv').config();
+module.exports ={
+    database:process.env.MONGOLAB_URI /*|| `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`*/,
+    secret: 'mysecret'
+  }

921fb42c04fb12a007c02af61923b959bd7d2f1c
Knowledge Kwaramba <knowhowkwaramba@gmail.com>
2020-11-28 10:53:59 +0200

    Update errors.js

--- apis/errors/errors.js
+++ apis/errors/errors.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const router = express.Router();
+let alert ={
+    'alert' : false,
 
+}
+
+router.get('/404',(req,res)=>{
+    res.render('./homefiles/errors/404',{
+        isAuthenticated: req.isAuthenticated(),
+        alert : alert
+    })
+});
+
+router.get('/500',(req,res)=>{
+    
+    res.render('./homefiles/errors/500',{
+        isAuthenticated: req.isAuthenticated(),
+        alert : alert
+    })
+});
+
+module.exports = router;
